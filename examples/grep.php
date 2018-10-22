0d55d39d9f8775687bcb5423894149a8b17a384b
Datagit <datagit@yahoo.com>
2018-10-22 21:29:53 +0700

    [Add mysql]

--- examples/grep.php
+++ examples/grep.php
@@ -1,14 +1,14 @@
 #!/usr/bin/env php
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use MrRio\ShellWrap as sh;
 
 $sh = new sh();
 
 try {
-    $curl = $sh->curl('https://raw.github.com/guumaster/sh/master/README.md' );
-    $grep = $sh->grep('ASDFInstallation', $curl);
+    $curl = $sh->curl('https://github.com/datagit/shellwrap/blob/master/README.md');
+    $grep = $sh->grep('ShellWrap', $curl);
     echo "match\n";
     echo $grep;
 } catch (Exception $e) {
